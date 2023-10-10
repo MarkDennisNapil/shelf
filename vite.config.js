ccb0e7e7a92894f332545710c8652f96d82c6794
Mark Dennis Napil <120807518+MarkDennisNapil@users.noreply.github.com>
2023-10-10 21:44:59 +0800

    docker create

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shelf/",
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8080
+  }
 })
